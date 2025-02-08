aa8682c77b2c3b9176b171917bf5c7a4197f7abb
ThomasTekie00 <thtek2805@uit.no>
2025-02-08 23:29:35 +0100

    Snart ferdig med lista

--- src/linkedlist.c
+++ src/linkedlist.c
@@ -1,7 +1,13 @@
 
 #include "../include/list.h"
 
+#include <cstddef>
 #include <stdlib.h>
+
+#include "../include/printing.h"
+
+
+
 
 
 typedef struct lnode lnode_t;
@@ -61,50 +67,183 @@ void list_destroy(list_t *list, free_fn item_free) {
     }
     
     lnode_t *curr = list -> head;
+    lnode_t *next_node;
+
+    while(curr != NULL){
+        next_node = curr -> next;
+        
+        if(item_free != NULL){
+            item_free(curr -> data);
+        }
+
+        free(curr);
+        curr = next_node;
+
+    }
+
+    free(list);
+
+    list -> length--;
 
 
     
 }
 
 size_t list_length(list_t *list) {
+    if (list == NULL){
+        return 0;
+    }
 
+    return list -> length;
 }
 
 int list_addfirst(list_t *list, void *item) {
-    lnode_t *node = newnode(item);
-
-    //Sette inn verdi
-    //
+    if (list == NULL){
+        return -1;
+    }
     
-    if//null
+    lnode_t *node = newnode(item);
+    if (node == NULL){
+        return -2;
+    }
+
+    if(list-> head == NULL){
+        list -> head = node;
+        list -> tail = node;
+    }else {
+    node -> next = list -> head;
+    list -> head -> prev = node;
     list -> head = node;
-    list -> tail = node;
 
-    if//det er andre
+    }
 
-    //returnere 0 eller negativ error 
+    list -> length++;
+
+    return 0;
+    
+    
 
 }
 
 int list_addlast(list_t *list, void *item) {
+    if(list == NULL){
+        return -1;
+    }
+
+    lnode_t *node = newnode(item);
+    if(node == NULL){
+        return -2;
+    }
+
+    if (list -> head == NULL){
+        list -> head = node;
+        list -> tail = node;
+    } else {
+        list -> tail -> next = node;
+        node -> prev = list -> tail;
+        list -> tail = node;
+    }
+
+    list -> length++;
+
+    return 0;
+
+
 
 }
 
 void *list_popfirst(list_t *list) {
+    if(list == NULL || list->head == NULL){
+        PANIC("List is empty");
+        return NULL;
+    }
 
+    lnode_t *temp = list -> head;
+    void *item = temp -> data;
+
+    list -> head = list -> head -> next;
+
+    if(list -> head != NULL){
+        list -> head -> prev = NULL;
+    }else{
+        list -> tail = NULL;
+    }
+
+    free(temp);
+    list -> length--;
+
+
+
+    return item;
+
+    
 }
 
 void *list_poplast(list_t *list) {
+    if(list == NULL || list -> tail == NULL){
+        PANIC("List is empty");
+        return NULL;
+    }
+
+    lnode_t *item = list -> tail -> data;
+    lnode_t *temp = list -> tail;
+
+    list -> tail = list -> tail -> prev;
+
+    if(list -> tail != NULL){
+        list -> tail -> next = NULL;
+    }else{
+        list -> head = NULL;
+    }
+
+    free(temp);
+    list -> length--;
+
+
+
+    return item;
+
 
 }
 
 int list_contains(list_t *list, void *item) {
+    if(list == NULL){
+        pr_error("List is empty, cannot check for item in list");
+        return 0;
+    }
 
+    lnode_t *curr = list -> head;
+    
+
+    while(curr != NULL){
+        if(list -> cmpfn(curr -> data, item) == 0){
+            return 1;
+        }else{
+            curr = curr -> next;
+        }
+    }
+    
+    return 0;
+  
 }
 
 void list_sort(list_t *list) {
+    if(list == NULL){
+        pr_error("Nothing to sort, list is empty");
+        return;
+    }
 
+    //Find the middel of the list
+    int mid = list_length(list) / 2;
+
+    lnode_t *mid_node = list -> head;
+    for(int i = 0; i < mid; i++){
+        mid_node = mid_node -> next;
+    }
+   
+    
 }
+
 
 list_iter_t *list_createiter(list_t *list) {
 
