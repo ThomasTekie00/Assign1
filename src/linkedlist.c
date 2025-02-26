8a242636e98467dde90c8bd4c2c778e9d4e7fb18
ThomasTekie00 <thtek2805@uit.no>
2025-02-26 13:10:56 +0100

    FERDIG

--- src/linkedlist.c
+++ src/linkedlist.c
@@ -6,416 +6,385 @@
 
 #include "../include/printing.h"
 
-
-
-
-
 typedef struct lnode lnode_t;
-struct lnode {
+struct lnode
+{
     void *data;
     lnode_t *next;
     lnode_t *prev;
 };
 
-struct list {
+struct list
+{
     lnode_t *head;
     lnode_t *tail;
     size_t length;
     cmp_fn cmpfn;
-    
 };
 
-struct list_iter {
+struct list_iter
+{
     list_t *list;
     lnode_t *node;
-    
-
 };
 
-
-static lnode_t *newnode(void *item) {
+static lnode_t *newnode(void *item)
+{
     lnode_t *node = malloc(sizeof(lnode_t));
-    if(node == NULL){
+    if (node == NULL)
+    {
         return NULL;
     }
 
-    node -> data = item;
-    node -> next = NULL;
-    node -> prev = NULL;
+    node->data = item;
+    node->next = NULL;
+    node->prev = NULL;
 
     return node;
-
 }
 
-list_t *list_create(cmp_fn cmpfn) {
+list_t *list_create(cmp_fn cmpfn)
+{
     list_t *list = malloc(sizeof(list_t));
-    if(list == NULL){
+    if (list == NULL)
+    {
         return NULL;
     }
 
-    list -> head = NULL;
-    list -> tail = NULL;
-    list -> cmpfn = cmpfn;
-    list -> length = 0;
+    list->head = NULL;
+    list->tail = NULL;
+    list->cmpfn = cmpfn;
+    list->length = 0;
 
     return list;
-
 }
 
-void list_destroy(list_t *list, free_fn item_free) {
-    if(list == NULL){
+void list_destroy(list_t *list, free_fn item_free)
+{
+    if (list == NULL)
+    {
         return;
     }
-    
-    lnode_t *curr = list -> head;
+
+    lnode_t *curr = list->head;
     lnode_t *next_node;
 
-    while(curr != NULL){
-        next_node = curr -> next;
-        
-        if(item_free != NULL){
-            item_free(curr -> data);
+    while (curr != NULL)
+    {
+        next_node = curr->next;
+
+        if (item_free != NULL)
+        {
+            item_free(curr->data);
         }
 
         free(curr);
         curr = next_node;
-
     }
 
     free(list);
-
-    
-
-
-    
 }
 
-size_t list_length(list_t *list) {
-    if (list == NULL){
+size_t list_length(list_t *list)
+{
+    if (list == NULL)
+    {
         return 0;
     }
 
-    return list -> length;
+    return list->length;
 }
 
-int list_addfirst(list_t *list, void *item) {
-    if (list == NULL){
-        return -1;
-    }
-    
-    lnode_t *node = newnode(item);
-    if (node == NULL){
-        return -2;
-    }
-
-    if(list-> head == NULL){
-        list -> head = node;
-        list -> tail = node;
-    }else {
-    node -> next = list -> head;
-    list -> head -> prev = node;
-    list -> head = node;
-
-    }
-
-    list -> length++;
-
-    return 0;
-    
-    
-
-}
-
-int list_addlast(list_t *list, void *item) {
-    if(list == NULL){
+int list_addfirst(list_t *list, void *item)
+{
+    if (list == NULL)
+    {
         return -1;
     }
 
     lnode_t *node = newnode(item);
-    if(node == NULL){
+    if (node == NULL)
+    {
         return -2;
     }
 
-    if (list -> head == NULL){
-        list -> head = node;
-        list -> tail = node;
-    } else {
-        list -> tail -> next = node;
-        node -> prev = list -> tail;
-        list -> tail = node;
+    if (list->head == NULL)
+    {
+        list->head = node;
+        list->tail = node;
+    }
+    else
+    {
+        node->next = list->head;
+        list->head->prev = node;
+        list->head = node;
     }
 
-    list -> length++;
+    list->length++;
 
     return 0;
-
-
-
 }
 
-void *list_popfirst(list_t *list) {
-    if(list == NULL || list->head == NULL){
+int list_addlast(list_t *list, void *item)
+{
+    if (list == NULL)
+    {
+        return -1;
+    }
+
+    lnode_t *node = newnode(item);
+    if (node == NULL)
+    {
+        return -2;
+    }
+
+    if (list->head == NULL)
+    {
+        list->head = node;
+        list->tail = node;
+    }
+    else
+    {
+        list->tail->next = node;
+        node->prev = list->tail;
+        list->tail = node;
+    }
+
+    list->length++;
+
+    return 0;
+}
+
+void *list_popfirst(list_t *list)
+{
+    if (list == NULL || list->head == NULL)
+    {
         PANIC("List is empty");
         return NULL;
     }
 
-    lnode_t *temp = list -> head;
-    void *item = temp -> data;
+    lnode_t *temp = list->head;
+    void *item = temp->data;
 
-    list -> head = list -> head -> next;
+    list->head = list->head->next;
 
-    if(list -> head != NULL){
-        list -> head -> prev = NULL;
-    }else{
-        list -> tail = NULL;
+    if (list->head != NULL)
+    {
+        list->head->prev = NULL;
+    }
+    else
+    {
+        list->tail = NULL;
     }
 
     free(temp);
-    list -> length--;
-
-
+    list->length--;
 
     return item;
-
-    
 }
 
-void *list_poplast(list_t *list) {
-    if(list == NULL || list -> tail == NULL){
+void *list_poplast(list_t *list)
+{
+    if (list == NULL || list->tail == NULL)
+    {
         PANIC("List is empty");
         return NULL;
     }
 
-    void *item = list -> tail -> data;
-    lnode_t *temp = list -> tail;
+    void *item = list->tail->data;
+    lnode_t *temp = list->tail;
 
-    list -> tail = list -> tail -> prev;
+    list->tail = list->tail->prev;
 
-    if(list -> tail != NULL){
-        list -> tail -> next = NULL;
-    }else{
-        list -> head = NULL;
+    if (list->tail != NULL)
+    {
+        list->tail->next = NULL;
+    }
+    else
+    {
+        list->head = NULL;
     }
 
     free(temp);
-    list -> length--;
-
-
+    list->length--;
 
     return item;
-
-
 }
 
-int list_contains(list_t *list, void *item) {
-    if(list == NULL){
+int list_contains(list_t *list, void *item)
+{
+    if (list == NULL)
+    {
         pr_error("List is empty, cannot check for item in list");
         return 0;
     }
 
-    lnode_t *curr = list -> head;
-    
+    lnode_t *curr = list->head;
 
-    while(curr != NULL){
-        if(list -> cmpfn(curr -> data, item) == 0){
+    while (curr != NULL)
+    {
+        if (list->cmpfn(curr->data, item) == 0)
+        {
             return 1;
-        }else{
-            curr = curr -> next;
+        }
+        else
+        {
+            curr = curr->next;
         }
     }
-    
+
     return 0;
-  
 }
 
-static void swap(lnode_t *a, lnode_t *b){
-    void *temp = a -> data;
-    a -> data = b -> data;
-    b -> data = temp;
+static void swap(lnode_t *a, lnode_t *b, list_t *list)
+{
+    if (a == NULL || b == NULL || a == b)
+    {
+        return;
+    }
 
+    void *temp = a->data;
+    a->data = b->data;
+    b->data = temp;
 }
 
-static lnode_t *list_tail(list_t *list, lnode_t *head, lnode_t *tail){
-    if(list == NULL || list->head == NULL){
-        pr_error("Cant give tail since list is empty!");
+static lnode_t *partition(lnode_t *head, lnode_t *tail, cmp_fn cmpfn, list_t *list)
+{
+    //
+    if (head == NULL || tail == NULL || cmpfn == NULL || list == NULL)
+    {
         return NULL;
     }
 
-    return list->tail;
-}
-
-
-static lnode_t *getmiddle(lnode_t *head, lnode_t *tail){
-    if(head == NULL || tail == NULL){
-        pr_error("List is empty");
-        return head;
-    }
-
-    lnode_t *slow = head;
-    lnode_t *fast = head;
-
-    while(fast != tail && fast -> next != NULL && fast -> next != tail){
-            fast = fast -> next -> next;
-            slow = slow -> next; 
-
-            if(fast == NULL){
-                break;
-            }
-    
-
-    }
-    return slow;//pass
-}
-  
-
-//Bruke cmpfn for å kutte liste i 2 for å legge til pivot #Eller bruke rndint og time
-static lnode_t *partition(lnode_t *head, lnode_t *tail, cmp_fn cmpfn){
-    if(head == NULL || tail == NULL || cmpfn == NULL){
-        return NULL;
-    }
-
-    lnode_t *pivot = getmiddle(head, tail);
-    if(pivot == NULL){
-        return NULL;
-    }
-
-    swap(pivot, tail);
-
-    void *pivotdata = tail -> data;
-    if(pivotdata == NULL){
-        return NULL;
-    }
-
-    lnode_t *i = NULL;
+    //
+    void *pivot = tail->data;
+    lnode_t *i = head->prev;
     lnode_t *j = head;
 
-    while(j != tail && j != NULL){
-        if (j -> data != NULL && cmpfn(j -> data, pivotdata) < 0) {
-            if(i == NULL){
+    //
+    while (j != tail)
+    {
+        if (cmpfn(j->data, pivot) < 0)
+        {
+            if (i == NULL)
+            {
                 i = head;
-            }else{
-                i = i -> next;
-                
             }
-            if(i != j){
-                swap(i, j);
+            else
+            {
+                i = i->next;
             }
-            
+            swap(i, j, list);
         }
-        j = j -> next;
+        j = j->next;
     }
-
-    if(i == NULL){
+    //
+    if (i == NULL)
+    {
         i = head;
-    }else{
-        i= i -> next;
     }
-
-    if(i != tail){
-        swap(i,tail);
+    else
+    {
+        i = i->next;
     }
-    
-
+    //
+    swap(i, tail, list);
     return i;
-    
-    
 }
-    
-   
-    
 
-static void sort_recursive(list_t *list, lnode_t *head, lnode_t *tail, cmp_fn cmpfn){
-    if(head == NULL || head == tail){
+//
+static void sort_recursive(list_t *list, lnode_t *head, lnode_t *tail, cmp_fn cmpfn)
+{
+    if (head == NULL || head == tail || tail == NULL)
+    {
         return;
     }
+    //
+    lnode_t *pivot = partition(head, tail, cmpfn, list);
 
-    lnode_t *pivot = partition(head, tail, cmpfn);
-
-    if(pivot != NULL && pivot -> prev != NULL){
-        sort_recursive(list, head, pivot -> prev, cmpfn);
+    //
+    if (pivot != head)
+    {
+        sort_recursive(list, head, pivot->prev, cmpfn);
     }
-
-    if(pivot != NULL && pivot -> next != NULL){
-        sort_recursive(list, pivot -> next, tail, cmpfn);
-
+    //
+    if (pivot != tail)
+    {
+        sort_recursive(list, pivot->next, tail, cmpfn);
     }
-    //pass
 }
 
-void list_sort(list_t *list){
-    if(list == NULL || list -> head == NULL){
+void list_sort(list_t *list)
+{
+    if (list == NULL || list->head == NULL || list->tail == NULL)
+    {
         return;
     }
-
-    sort_recursive(list, list -> head, list -> tail, list -> cmpfn);
-
+    //
+    sort_recursive(list, list->head, list->tail, list->cmpfn);
 }
-    //Gjøres
 
-
-  
-    
-
-
-
-list_iter_t *list_createiter(list_t *list) {
+list_iter_t *list_createiter(list_t *list)
+{
     list_iter_t *iter = malloc(sizeof(list_iter_t));
-    if(iter == NULL){
+    if (iter == NULL)
+    {
         return NULL;
     }
-    
-    iter -> list = list;
-    iter -> node = list -> head;
+
+    iter->list = list;
+    iter->node = list->head;
 
     return iter;
-
 }
 
-void list_destroyiter(list_iter_t *iter) {
-    if(iter != NULL){
+void list_destroyiter(list_iter_t *iter)
+{
+    if (iter != NULL)
+    {
         free(iter);
     }
-    
-    
 }
 
-int list_hasnext(list_iter_t *iter) {
-    //Check if iterator or current node is NULL
-    if(iter == NULL || iter -> node == NULL){
-        //Done with the round
+int list_hasnext(list_iter_t *iter)
+{
+    // Check if iterator or current node is NULL
+    if (iter == NULL || iter->node == NULL)
+    {
+        // Done with the round
         return 0;
     }
-    //More to iterate
+    // More to iterate
     return 1;
-
 }
 
-
-void *list_next(list_iter_t *iter) {
-    if(iter == NULL || iter -> node == NULL){
+void *list_next(list_iter_t *iter)
+{
+    if (iter == NULL || iter->node == NULL)
+    {
         return NULL;
     }
 
-    //Get the value from current node
-    void *item = iter -> node -> data;
+    // Get the value from current node
+    void *item = iter->node->data;
 
-    //Move iter to next node
-    iter -> node = iter -> node -> next;
+    // Move iter to next node
+    iter->node = iter->node->next;
 
     return item;
-
 }
 
-void list_resetiter(list_iter_t *iter) {
-    if(iter == NULL){
+void list_resetiter(list_iter_t *iter)
+{
+    if (iter == NULL)
+    {
         return;
     }
-    //Reseter: Goes back to the head of the list
-    if(iter -> list != NULL){
-        iter -> node = iter -> list -> head;
+    // Reseter: Goes back to the head of the list
+    if (iter->list != NULL)
+    {
+        iter->node = iter->list->head;
     }
 
-   // iter -> node = iter -> list -> head;    
-
-    
+    // iter -> node = iter -> list -> head;
 }
